2561422c7ef55644f20373f10d9ad50d17ce40f7
franciele19 <francielesy@gmail.com>
2022-10-08 22:51:08 -0300

    new

---
+++ Simulador/ScriptsAuxiliares/parametros_simulacao.m
@@ -0,0 +1,16 @@
+FilteringTimeConstant = 0.001; %s
+gravidade = [0 0 -9.80665]; %m/s^2
+
+SphereRadius = 1e-4; %m
+chao = [0.5 1 0.01]; %m
+OffsetChao = [0 -0.4 0]; %m
+ContactStiffness = 25000; %N/m
+ContactDamping = 100; %Ns/m
+KinectFriction = 0.6;
+StaticFriction = 0.8;
+VelocityThreshold = 0.1; %m/s
+
+CoefAngAX = -693/1475/pi; %Nms/rad
+CoefLinAX = 0.924; %Nm
+CoefAngMX = -21/25/pi; %Nms/rad
+CoefLinMX = 77/50; %Nm
